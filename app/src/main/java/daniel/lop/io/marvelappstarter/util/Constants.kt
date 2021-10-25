a755a575f4e86631d70eb1224338a690f1f46b84
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-10-25 15:28:19 -0400

    Criando interface - ServiceApi.

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,9 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+
+    const val PUBLIC_KEY = "d2bc6bdd347416a17a87e9a74a4299fb"
+    const val PRIVATE_KEY = "892716b3c5891b1ad705431850980b9d86dd74e7"
+
+}
