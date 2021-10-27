5b15e95058d4b881b5c5d4fa48e1abab0460392a
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-10-27 11:22:01 -0400

    Fragment

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -6,6 +6,9 @@ object Constants {
     const val PUBLIC_KEY = "d2bc6bdd347416a17a87e9a74a4299fb"
     const val PRIVATE_KEY = "892716b3c5891b1ad705431850980b9d86dd74e7"
 
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
+
     const val APIKEY = "apikey"
     const val HASH = "hash"
     const val TS = "ts"
