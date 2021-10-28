756e4ac331820bdd8b1df9d35a3b5b7cb1763be0
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-10-28 17:28:23 -0400

    Injeção de dependência - Room.

--- app/src/main/java/daniel/lop/io/marvelappstarter/repository/MarvelRepository.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/repository/MarvelRepository.kt
@@ -1,12 +1,19 @@
 package daniel.lop.io.marvelappstarter.repository
 
+import daniel.lop.io.marvelappstarter.data.local.MarvelDao
+import daniel.lop.io.marvelappstarter.data.model.character.CharacterModel
 import daniel.lop.io.marvelappstarter.data.remote.ServiceApi
 import java.sql.RowId
 import javax.inject.Inject
 
 class MarvelRepository @Inject constructor(
-    private val api: ServiceApi
+    private val api: ServiceApi,
+    private val dao: MarvelDao
 ) {
-    suspend fun list (nameStartsWith: String? = null) = api.list(nameStartsWith)
+    suspend fun list(nameStartsWith: String? = null) = api.list(nameStartsWith)
     suspend fun getComics(characterId: Int) = api.getComics(characterId)
+
+    suspend fun insert(characterModel: CharacterModel) = dao.insert(characterModel)
+    fun getAll() = dao.getAll()
+    suspend fun delete(characterModel: CharacterModel) = dao.delete((characterModel))
 }
--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -1,6 +1,7 @@
 package daniel.lop.io.marvelappstarter.util
 
 object Constants {
+    const val DATABASE_NAME = "marvel.db"
     const val BASE_URL = "https://gateway.marvel.com/v1/public/"
 
     const val PUBLIC_KEY = "d2bc6bdd347416a17a87e9a74a4299fb"
